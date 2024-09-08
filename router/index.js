2b7a487d426df270627247c856a087c0b22158a7
ArmenHovsepyan01 <arh20025@gmail.com>
2024-09-09 00:15:02 +0400

    removed unused route

--- router/index.js
+++ router/index.js
@@ -4,12 +4,6 @@ import sendMail from "../controllers/send-mail/index.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.json({
-        message: "Welcome!!",
-    })
-});
-
 router.post("/send-mail", authenticateUser, sendMail);
 
 export default router;
